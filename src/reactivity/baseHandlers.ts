2720109ddec697d099bfd002e27be6eb09945d38
潘静怡 <1003346758@qq.com>
2023-05-03 02:17:26 +0800

    feat: 一些代码优化

--- src/reactivity/baseHandlers.ts
+++ src/reactivity/baseHandlers.ts
@@ -1,4 +1,5 @@
 import { track, trigger } from "./effect"
+import { ReactiveFlags } from "./reactivity"
 
 const get = createGetter()
 const set = createSetter() //保证初始化的时候调用一次，后序都不在调用
@@ -6,6 +7,12 @@ const readonlyGet = createGetter(true)
 
 function createGetter(isReadonly = false){
   return function get(target, key){
+    if(key === ReactiveFlags.IS_REACTIVE){
+      return !isReadonly
+    } else if(key === ReactiveFlags.IS_READONLY){
+      return isReadonly
+    }
+
     const res = Reflect.get(target, key)
 
     if(!isReadonly){
