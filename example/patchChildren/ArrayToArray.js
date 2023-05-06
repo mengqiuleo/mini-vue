e9ba6b715ec9af8ab55742716a1f6aa134d22be2
潘静怡 <1003346758@qq.com>
2023-05-07 02:22:46 +0800

    diff

--- example/patchChildren/ArrayToArray.js
+++ example/patchChildren/ArrayToArray.js
@@ -87,15 +87,85 @@ import {ref, h } from '../../lib/guide-mini-vue.esm.js'
 // a (b c)
 // (b c)
 // i = 0, e1 = 0, e2 = -1
+// const prevChildren = [
+//   h('p', { key: 'A' }, 'A'),
+//   h('p', { key: 'B' }, 'B'),
+//   h('p', { key: 'C' }, 'C')
+// ]
+
+// const nextChildren = [
+//   h('p', { key: 'B' }, 'B'),
+//   h('p', { key: 'C' }, 'C'),
+// ]
+
+//5.对比中间的部分
+// 删除老的
+// a b (c d) f g
+// a b (e c) f g
+// const prevChildren = [
+//   h('p', { key: 'A' }, 'A'),
+//   h('p', { key: 'B' }, 'B'),
+//   h('p', { key: 'C', id: 'c-prev' }, 'C'),
+//   h('p', { key: 'D' }, 'D'),
+//   h('p', { key: 'F' }, 'F'),
+//   h('p', { key: 'G' }, 'G')
+// ]
+
+// const nextChildren = [
+//   h('p', { key: 'A' }, 'A'),
+//   h('p', { key: 'B' }, 'B'),
+//   h('p', { key: 'E' }, 'E'),
+//   h('p', { key: 'C', id: 'c-next' }, 'C'),
+//   h('p', { key: 'F' }, 'F'),
+//   h('p', { key: 'G' }, 'G')
+// ]
+
+// 乱序：移动
+//a b (c d e) f g
+//a b (e c d) f g
+// const prevChildren = [
+//   h('p', { key: 'A' }, 'A'),
+//   h('p', { key: 'B' }, 'B'),
+//   h('p', { key: 'C' }, 'C'),
+//   h('p', { key: 'D' }, 'D'),
+//   h('p', { key: 'E' }, 'E'),
+//   h('p', { key: 'F' }, 'F'),
+//   h('p', { key: 'G' }, 'G')
+// ]
+
+// const nextChildren = [
+//   h('p', { key: 'A' }, 'A'),
+//   h('p', { key: 'B' }, 'B'),
+//   h('p', { key: 'E' }, 'E'),
+//   h('p', { key: 'C' }, 'C'),
+//   h('p', { key: 'D' }, 'D'),
+//   h('p', { key: 'F' }, 'F'),
+//   h('p', { key: 'G' }, 'G')
+// ]
+
+//综合
+// a b (c d e z) f g
+// a b (d c y e) f g
 const prevChildren = [
   h('p', { key: 'A' }, 'A'),
   h('p', { key: 'B' }, 'B'),
-  h('p', { key: 'C' }, 'C')
+  h('p', { key: 'C' }, 'C'),
+  h('p', { key: 'D' }, 'D'),
+  h('p', { key: 'E' }, 'E'),
+  h('p', { key: 'Z' }, 'Z'),
+  h('p', { key: 'F' }, 'F'),
+  h('p', { key: 'G' }, 'G')
 ]
 
 const nextChildren = [
+  h('p', { key: 'A' }, 'A'),
   h('p', { key: 'B' }, 'B'),
+  h('p', { key: 'D' }, 'D'),
   h('p', { key: 'C' }, 'C'),
+  h('p', { key: 'Y' }, 'Y'),
+  h('p', { key: 'E' }, 'E'),
+  h('p', { key: 'F' }, 'F'),
+  h('p', { key: 'G' }, 'G')
 ]
 
 export default {
