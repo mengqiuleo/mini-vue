1c04e2a4ae7e627b6b52874f4be96e47b08265a6
潘静怡 <1003346758@qq.com>
2023-05-08 03:31:18 +0800

    feat:实现解析三种联合类型

--- src/compiler-core/tests/parse.spec.ts
+++ src/compiler-core/tests/parse.spec.ts
@@ -21,7 +21,8 @@ describe('Parse', () => {
       const ast = baseParse('<div></div>')
       expect(ast.children[0]).toStrictEqual({
         type: NodeType.ELEMENT,
-        tag: 'div'
+        tag: 'div',
+        children: []
       })
     })
   }),
@@ -34,5 +35,59 @@ describe('Parse', () => {
         content: 'some text'
       })
     })
+  }),
+
+  test('hello world', () => {
+    const ast = baseParse('<div>hi, {{message}}</div>')
+    expect(ast.children[0]).toStrictEqual({
+      type: NodeType.ELEMENT,
+      tag: 'div',
+      children: [
+        {
+          type: NodeType.TEXT,
+          content: 'hi, '
+        },
+        {
+          type: NodeType.INTERPOLATION,
+          content: {
+            type: NodeType.SIMPLE_EXPRESSION,
+            content: 'message'
+          }
+        }
+      ]
+    })
+  }),
+
+  test('Nested element', () => {
+    const ast = baseParse('<div><p>hi</p>{{message}}</div>')
+    expect(ast.children[0]).toStrictEqual({
+      type: NodeType.ELEMENT,
+      tag: 'div',
+      children: [
+        {
+          type: NodeType.ELEMENT,
+          tag: 'p',
+          children: [
+            {
+              type: NodeType.TEXT,
+              content: 'hi',
+            }
+          ]
+        },
+        {
+          type: NodeType.INTERPOLATION,
+          content: {
+            type: NodeType.SIMPLE_EXPRESSION,
+            content: 'message'
+          }
+        }
+      ]
+    })
+  }),
+
+  test('should throw error when lack end tag', () => {
+    expect(() => {
+      baseParse('<div><span></div>')
+    }).toThrow('缺少结束标签:span')
   })
 })
